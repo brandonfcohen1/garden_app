5a6b825e536c20dbb57ba1419fbf83190ad27219
brandonfcohen1 <brandon.f.cohen@gmail.com>
2020-04-07 21:05:53 -0400

    got motor code working

--- sensor_test.py
+++ sensor_test.py
@@ -1,1 +1,14 @@
-x=1
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+ 
+port = 14
+GPIO.setup(port, GPIO.OUT)
+
+#Turn on
+GPIO.output(port,0)
+time.sleep(3)
+
+#Turn off
+GPIO.output(port,1)
