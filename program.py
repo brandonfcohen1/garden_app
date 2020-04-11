5b2dbbda01905afa24114561890ce331ba54616f
brandonfcohen1 <brandon.f.cohen@gmail.com>
2020-04-10 20:34:02 -0400

    dataframe object

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from sensors.barometric_sensor import *
 from sensors.cpu_temp import *
 from sensors.rpio_sensors import *
@@ -8,11 +9,11 @@ from sensors.humidity_sensor import *
 HUMIDITY_SENSOR = 15
 
 
+
 def get_all_readings():
     baro = read_barometric()
     cpu_temp = get_cpu_temp()
     humid = DHT11(pin = HUMIDITY_SENSOR).read().return_results()
     light = read_light()
-    return({'baro_temp':baro[0], 'baro_pressure': baro[1], 'cpu_temp': cpu_temp, 'humid_temp': humid[0], 'humid_humid': humid[1], 'light': light})
+    return(pd.DataFrame({'baro_temp':baro[0], 'baro_pressure': baro[1], 'cpu_temp': cpu_temp, 'humid_temp': humid[0], 'humid_humid': humid[1], 'light': light}, index = [time.time()]))
 
-x = get_all_readings()
