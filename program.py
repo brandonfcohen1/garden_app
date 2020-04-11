9137392ea0c9d5d1bce27128cf8333f4be98da2c
brandonfcohen1 <brandon.f.cohen@gmail.com>
2020-04-10 20:09:04 -0400

    got dict working

--- program.py
+++ program.py
@@ -13,6 +13,6 @@ def get_all_readings():
     cpu_temp = get_cpu_temp()
     humid = DHT11(pin = HUMIDITY_SENSOR).read().return_results()
     light = read_light()
-    return(baro, cpu_temp, humid, light)
+    return({'baro_temp':baro[0], 'baro_pressure': baro[1], 'cpu_temp': cpu_temp, 'humid_temp': humid[0], 'humid_humid': humid[1], 'light': light})
 
 x = get_all_readings()
